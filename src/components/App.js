0445989c77e9e636f41367b215370d038c4dd426
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-11-21 08:54:23 -0800

    Header added and program running properly on localhost.

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from "./Header";
+
+function App() {
+  return (
+  <React.Fragment>
+    <Header />
+    <div class = "container">
+      <div class="row">
+        <div class="col-md-3">
+        </div>
+        <div class="col-md-3">
+        </div>
+      </div>
+    </div>
+
+  </React.Fragment>
+  );
+}
+
+export default App; 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header (){
+  return (
+    <React.Fragment>
+    <div class="container">
+      <h1>Tap Room</h1>
+      {/* <img class="pic" src="https://i.pinimg.com/564x/c5/8a/d4/c58ad46c9e1a1d3272ab6eb4b55dc06f.jpg"/> */}
+      <hr/>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default Header;

5ca2c7848ed7096178362f1f49e9149bed6b3dc4
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-12-04 09:01:12 -0800

    Delete keg test built and successfully failed.

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -11,6 +11,19 @@ let action;
     id: 1
   };
 
+  const currentState = {
+    1: {name: 'Giuseppe Warlock Double IPA',
+    brand: 'Breakside Brewery',
+    price: '$5.00',
+    style: 'Double India Pale Ale',
+    id: 1 },
+    2: {name: 'Salted Carmel Stout',
+    brand: 'Breakside Brewery',
+    price: '$6.00',
+    style: 'Stout',
+    id: 2 }
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null})).toEqual({});
   });
@@ -33,6 +46,20 @@ let action;
         style: style,
         id: id
       }
+    });
+  });
+
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {name: 'Salted Carmel Stout',
+      brand: 'Breakside Brewery',
+      price: '$6.00',
+      style: 'Stout',
+      id: 2 }
     });
   });
 });

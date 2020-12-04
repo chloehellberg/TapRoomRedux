444bd8292e20626c0e534dba3540dca89554b136
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-12-04 08:34:10 -0800

    Add second test for adding new keg to menu/list.

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -2,7 +2,37 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
 
+let action;
+  const kegData = {
+    name: 'Starstruck',
+    brand: 'Breakside Brewery',
+    price: '$5.00',
+    style: 'India Pale Ale',
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null})).toEqual({});
   });
-})
+
+  test('Should successfully add new keg data to the masterKegList', () => {
+    const { name, brand, price, style, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      style: style,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        style: style,
+        id: id
+      }
+    });
+  });
+});
